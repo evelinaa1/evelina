ff7931c7c251a8cdc29cc94236484e797d90f230
evelinaa1 <eveliinaskola5@gmail.com>
2025-01-21 09:46:33 +0000

    darbs02

--- 2.py
+++ 2.py
@@ -1,1 +1,8 @@
-print()
+skaitlis = int(input("Ievadiet skaitli:"))
+print (type(skaitlis))
+if skaitlis>0:
+    print("Skaitlis pozitīvs")
+elif skaitlis==0:
+    print("Skaitlis ir nulle")
+else:
+    print("Skatilis negatīvs")
