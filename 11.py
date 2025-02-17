4cb63bcf278221acb304889e0ad3aaae694b8211
evelinaa1 <eveliinaskola5@gmail.com>
2025-02-17 14:07:35 +0000

    darbs11

---
+++ 11.py
@@ -0,0 +1,11 @@
+teikums1=input()
+ir_drukats=False
+for in range(len(teikums1)):
+    if teikums1[i]>= 'A' and teikums1[i]<='Z':
+        print("IR DRUKATAIS")
+        break
+if ir_drukats==False:
+ print("NAV DRUKATAIS")
+
+
+
