cdedfdfadbb9ff7c2d7d7647f25b01ac843839a2
evelinaa1 <eveliinaskola5@gmail.com>
2025-02-18 10:02:02 +0000

    15.py

---
+++ 14.py
@@ -0,0 +1,10 @@
+teikums = input()
+
+
+    vards = [vardi.lower()for vards in teikums.split()]
+    vardi.sort()
+    
+    print("Alfabētskā secībā vārdi:")
+for vardi in vards:
+   print(vardi)
+
---
+++ 15.py
@@ -0,0 +1,17 @@
+teikums = input()
+vardi = teikums.split(" ")
+for vards in vardi:
+ for i in range(len(teikums)):
+   if teikums == 3 vardi in teikums:
+  print ("IR 3 SIMBOLI")
+
+else:
+  print ("NAV 3 SIMBOLI")
+
+  if len(vards) == 3:
+   print (" ")
+
+else:
+  print("NAV TĀDU VĀRDU")
+
+ 
